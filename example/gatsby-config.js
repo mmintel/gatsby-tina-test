731085d7ced709f98738a2e4a9fc21a462c8a9e2
Nolan Phillips <ncphillips.19@gmail.com>
2020-03-14 11:21:49 -0300

    chore: configure example site to work with gatsby-tinacms-git

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,8 +1,16 @@
+const path = require('path')
+
+const MONOREPO_PATH = path.resolve(process.cwd(), '..')
+
+console.log({ MONOREPO_PATH})
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-minimal`,
       options: {
+        pathToRepo: MONOREPO_PATH,
+        pathToContent: 'example'
       }
     }
   ],
